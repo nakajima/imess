59b73076125c40269dcd0e8270e722a4743d58ca
Scott J. Goldman <scottjg@github.com>
2014-04-15 00:46:32 -0700

    hook up failure detection

--- app.rb
+++ app.rb
@@ -6,10 +6,16 @@ class Messager < Sinatra::Application
   end
 
   post '/messages' do
-    begin
-      system "osascript", "message.applescript", params[:phone], params[:message]
-    rescue => e
-      p e
+    system "osascript", "message.applescript", params[:phone], params[:message]
+    sleep 2
+    system "screencapture -l$(osascript -e 'tell app \"Messages\" to id of window 1') /tmp/test.png"
+    pixel_info1=`convert /tmp/test.png -crop '1x1+296+484' txt:-`
+    #pixel_info2=`convert /tmp/test.png -crop '1x1+200+110' txt:-`
+    puts pixel_info1
+    #puts pixel_info2
+    if !(pixel_info1 =~ /#A1A2C3 /)
+      system "osascript -e 'tell application \"Messages\" keystroke return'"
+      raise "failed :("
     end
   end
 end
